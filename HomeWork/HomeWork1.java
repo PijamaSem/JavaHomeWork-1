cb7c45a718efd903d6daaf10821e98d06ea792ab
Your Name <you@example.com>
2023-02-18 21:03:48 +0300

    Input

---
+++ HomeWork/HomeWork1.java
@@ -0,0 +1,24 @@
+package Java.Seminar1.HomeWork;
+
+import java.util.Scanner;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        int num = UserInput();
+        System.out.println(num);
+        // int result = TringleDigitCalculation(num);
+        // WTF(result);
+        
+    }
+    public static int UserInput() {
+        Scanner iScanner = new Scanner(System.in);
+        // Scanner iScanner = new Scanner(System.in);
+        System.out.println("Введите порядок треугольного числа для расчета:");
+        String input = iScanner.nextLine();
+        int result = Integer.parseInt(input);
+        // System.out.printf("%d Хороший выбор!", result);
+        // System.out.println();
+        iScanner.close();
+        return result;
+    }
+}
